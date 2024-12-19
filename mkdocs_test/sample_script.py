fc188a73396b8db27d79f5ca26f46cd36b2d098d
Vetle Nevland <89903568+vetlenev@users.noreply.github.com>
2024-12-19 11:11:37 +0000

    finalized setup of sample mkdocs

---
+++ mkdocs_test/sample_script.py
@@ -0,0 +1,12 @@
+def my_func(a: str, b: int) -> int:
+    """Sample function.
+
+    Args:
+        a: A string.
+        b: An integer.
+
+    Returns:
+        int: The sum of a and b.
+
+    """
+    return int(a) + b
